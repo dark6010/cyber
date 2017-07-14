4a591b71a489d049e5858ef4c96d196d97a8781b
Grover Plaza Quiroga <grover.plaza@gmail.com>
2017-07-14 01:10:30 -0400

    dd

--- controllers/proc.js
+++ controllers/proc.js
@@ -1,6 +1,7 @@
 var ListaModel = require('../models/ListaModel.js');
 var ProductoModel = require('../models/ProductoModel.js');
 var RegistroModel = require('../models/RegistroModel.js');
+var mongoose = require('mongoose');
 module.exports = {
   regProducto: function(req, res){
     res.render('regProducto')
@@ -30,11 +31,19 @@ module.exports = {
                     error: err
                 });
             }
-            console.log(Productos)
-            res.render ('regLista', Productos);
+            var p={}
+            p.productos=Productos
+            res.render ('regLista', p);
         });
   },
   postregLista: function(req, res){
+    if(req.body.p){
+      for(var i=0; i<req.body.p.length; i++){
+///        var id = mongoose.Types.ObjectId();
+      }
+    }else{
+      res.render(error, {message: "la lsita esta vacia o hubo algun error"})
+    }
     
   },
   p1: function(req, res){
